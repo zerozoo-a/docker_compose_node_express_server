8386c659aa3170c3b27bf7672e2fa7a31aac1532
zerozoo <zerozoo385@gmail.com>
2024-02-14 01:41:48 +0900

    add mock data need type

---
+++ src/controllers/mock.controller.js
@@ -0,0 +1,14 @@
+/**
+ * @typedef {import('express').Router.get} get
+ */
+
+/**
+ * This function handles a GET request.
+ * @type {get} req - The request object.
+ */
+
+const index = (req, res) => {
+  res;
+};
+
+export { index };
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,10 @@ import { index } from "../controllers/homeController.js";
 
 const router = Router();
 
+router.get("/aa", function a(req, res) {
+  res.json({ a: 1, b: 2, c: 3 });
+});
 router.get("/", index);
+router.get("/mock", (req, res) => {});
 
 export default router;
